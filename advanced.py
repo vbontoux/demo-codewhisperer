d950190d2491a681272e41dc82675a128545035c
Vincent Bontoux <v.bontoux@free.fr>
2023-11-17 07:40:02 +0100

    added advanced use case (generating a class and subclass)

---
+++ advanced.py
@@ -0,0 +1,8 @@
+# create a class called BankAccount that represents a bank account.
+# add attributes such as account number, balance and account holder
+# add a constructor that initializes these attributes
+# add methods called deposit and withdraw that add or subtracts an amount from the balance
+
+
+
+# create a subclass called CheckingAccount that inherits from BankAccount
